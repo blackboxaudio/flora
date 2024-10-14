0c76e15eca489a546ecc79e61fe714926ef912e4
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-10-13 19:37:28 -0500

    Add libs

---
+++ patches/simple_oscillator/simple_oscillator.cpp
@@ -0,0 +1,2 @@
+#include "daisy_patch_sm.h"
+#include "daisysp.h"
--- src/flora.h
+++
@@ -1,8 +0,0 @@
-/**
-* Flora is a collection of patches for the Electrosmith Daisy devices.
-*
-* Author: Matthew Maxwell, 2024
-*/
-#pragma once
-
-#include "sample.h"

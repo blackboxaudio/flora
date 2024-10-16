d2ac9df6e81b2d07975a94ddfa144c686c875e3b
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-10-16 14:47:41 -0500

    Add working example

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,18 +3,18 @@
 CORTEX_DIR=$PWD/lib/cortex
 DAISY_DIR=$PWD/lib/libDaisy
 
-printf "Building Cortex\n"
+printf "Building Cortex...\n"
 cd $CORTEX_DIR; ./scripts/build.sh release
 if [ $? -ne 0 ]; then
     printf "Failed to compile Cortex library\n"
     exit 1
 fi
 
-printf "\nBuilding Daisy\n"
+printf "\nBuilding Daisy...\n"
 cd $DAISY_DIR; make -s clean; make -j -s
 if [ $? -ne 0 ]; then
     printf "Failed to compile Daisy hardware library\n"
     exit 1
 fi
 
-printf "\Done.\n"
+printf "Done.\n"

6080bcfe48ef229f223c8470d918edd9a0f647b3
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-10-16 16:52:09 -0500

    Add CI workflow for building patches

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,17 +4,17 @@ CORTEX_DIR=$PWD/lib/cortex
 DAISY_DIR=$PWD/lib/libDaisy
 
 printf "Building Cortex...\n"
-cd $CORTEX_DIR; ./scripts/build.sh release
+cd $CORTEX_DIR; make -s clean; make -j -s
 if [ $? -ne 0 ]; then
     printf "Failed to compile Cortex library\n"
     exit 1
 fi
 
-printf "\nBuilding Daisy...\n"
+printf "Building Daisy...\n"
 cd $DAISY_DIR; make -s clean; make -j -s
 if [ $? -ne 0 ]; then
     printf "Failed to compile Daisy hardware library\n"
     exit 1
 fi
 
-printf "Done.\n"
+printf "\nDone.\n"

fff041655e9fc130726b490042b2a72eaa7ccf9d
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-10-17 18:14:35 -0500

    Get basic working example

--- patches/init/kudzu/kudzu.cpp
+++ patches/init/kudzu/kudzu.cpp
@@ -11,12 +11,22 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
     AudioHandle::InterleavingOutputBuffer out,
     size_t size)
 {
+    hardware.ProcessAllControls();
+
     float driveKnob = hardware.GetAdcValue(CV_1);
-    saturator.SetSaturationLevel(driveKnob + 1.0f);
+    float driveCv = 1.0f; // hardware.GetAdcValue(CV_5);
+    const float MAX_SATURATION = 24.0f;
+    saturator.SetSaturation((driveKnob * driveCv * MAX_SATURATION) + 1.0f);
+
+    float mixKnob = hardware.GetAdcValue(CV_4);
 
     for (size_t idx = 0; idx < size; idx += 2) {
-        out[idx] = (float)saturator.Process(in[idx]);
-        out[idx + 1] = (float)saturator.Process(in[idx + 1]);
+        float originalLeftSample = in[idx];
+        float originalRightSample = in[idx + 1];
+        auto saturatedLeftSample = (float)saturator.Process(originalLeftSample);
+        auto saturatedRightSample = (float)saturator.Process(originalRightSample);
+        out[idx] = originalLeftSample * (1.0f - mixKnob) + saturatedLeftSample * mixKnob;
+        out[idx + 1] = originalRightSample * (1.0f - mixKnob) + saturatedRightSample * mixKnob;
     }
 }
 
